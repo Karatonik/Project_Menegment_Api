d0630513f7ca32bed8f0734a63199f9dd0ad6d87
Karaton <mat.kalksztejn@wp.pl>
2022-05-31 20:45:24 +0200

    test12

--- src/main/java/pl/project/ProjectManagement/controller/TaskController.java
+++ src/main/java/pl/project/ProjectManagement/controller/TaskController.java
@@ -45,12 +45,11 @@ public class TaskController {
     public ResponseEntity<?> getProjectTasks(@RequestHeader("Authorization") String authorization,
                                              @PathVariable long projectId,
                                              Pageable pageable) {
-        System.out.println(pageable);
         List<TaskDto> tasks = this.taskService
                 .getProjectTasks(this.infoService.getEmailFromJwt(authorization), projectId)
                 .stream().map(TaskDto::new).toList();
 
-        int start =(int)  pageable.getOffset()*pageable.getPageSize();
+        int start =Math.min( pageable.getPageNumber()*pageable.getPageSize(),tasks.size()-1);
         int end = Math.min(start + pageable.getPageSize(),tasks.size()-1);
         System.out.println(start);
         System.out.println(end);
